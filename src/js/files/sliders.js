cadd3e33984a98c9b49febec23fedbfdb4daa3c8
d3n2dboy <d3n2dboy@gmail.com>
2025-04-02 22:04:58 +0500

    first commit

---
+++ src/js/files/sliders.js
@@ -0,0 +1,107 @@
+/*
+Документация по работе в шаблоне: 
+Документация слайдера: https://swiperjs.com/
+Сниппет(HTML): swiper
+*/
+
+// Подключаем слайдер Swiper из node_modules
+// При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
+// Пример: { Navigation, Autoplay }
+import Swiper, { EffectCoverflow, EffectCreative, Navigation } from 'swiper';
+/*
+Основниые модули слайдера:
+Navigation, Pagination, Autoplay, 
+EffectFade, Lazy, Manipulation
+Подробнее смотри https://swiperjs.com/
+*/
+
+// Стили Swiper
+// Базовые стили
+import "../../scss/base/swiper.scss";
+// Полный набор стилей из scss/libs/swiper.scss
+import "../../scss/libs/swiper.scss";
+// Полный набор стилей из node_modules
+import 'swiper/css';
+
+// Инициализация слайдеров
+function initSliders() {
+	// Перечень слайдеров
+	// Проверяем, есть ли слайдер на стронице
+	if (document.querySelector('.swiper')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.swiper', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation, EffectCoverflow],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 3.5,
+			spaceBetween: 20,
+			//autoHeight: true,
+			speed: 800,
+
+			//centeredSlides: true, //первый слайд по центру
+			//centeredSlidesBounds: true,
+
+			// effect: 'coverflow',
+			// coverflowEffect: {
+			// 	rotate: 1,
+			// 	slideShadows: false,
+			// 	stretch: 0,
+			// 	centeredSlides: false,
+			// 	//slidesPerView: "auto",
+			// },
+
+			effect: 'creative',
+			creativeEffect: {
+				prev: {
+					// will set `translateZ(-400px)` on previous slides
+					//translate: [0, 0, -400],
+				},
+				next: {
+					// will set `translateX(100%)` on next slides
+					//translate: ['100%', 0, 0],
+				},
+			},
+			// События
+			on: {
+
+			}
+		});
+	}
+}
+// Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
+function initSlidersScroll() {
+	let sliderScrollItems = document.querySelectorAll('.swiper_scroll');
+	if (sliderScrollItems.length > 0) {
+		for (let index = 0; index < sliderScrollItems.length; index++) {
+			const sliderScrollItem = sliderScrollItems[index];
+			const sliderScrollBar = sliderScrollItem.querySelector('.swiper-scrollbar');
+			const sliderScroll = new Swiper(sliderScrollItem, {
+				observer: true,
+				observeParents: true,
+				direction: 'vertical',
+				slidesPerView: 'auto',
+				freeMode: {
+					enabled: true,
+				},
+				scrollbar: {
+					el: sliderScrollBar,
+					draggable: true,
+					snapOnRelease: false
+				},
+				mousewheel: {
+					releaseOnEdges: true,
+				},
+			});
+			sliderScroll.scrollbar.updateSize();
+		}
+	}
+}
+
+window.addEventListener("load", function (e) {
+	// Запуск инициализации слайдеров
+	initSliders();
+	// Запуск инициализации скролла на базе слайдера (по классу swiper_scroll)
+	//initSlidersScroll();
+});
